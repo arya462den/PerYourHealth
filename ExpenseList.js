389f004a9c9d5391505ab4667a63e2281d74a07f
arya462den <arya462den@outlook.com>
2025-07-30 09:24:32 +0530

    Create ExpenseList.js

---
+++ ExpenseList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ExpenseList.css';
+
+function ExpenseList() {
+  return (
+    <div>
+      <h2>Expense List</h2>
+      <ul>
+        <li>$100 - Consultation</li>
+        <li>$250 - Medication</li>
+      </ul>
+    </div>
+  );
+}
+
+export default ExpenseList;
